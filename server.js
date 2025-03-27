2e877972a2cf4123e7ba64abf7b4fcb2f9df0af5
Corruptt4 <corruptt4@gmail.com>
2025-03-27 16:05:13 +0200

    a

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require("express")
 ,   { createServer } = require("node:http")
 ,   app = express()
 ,   server = createServer(app)
+, { Server } = require("socket.io")
+, io = new Server(server)
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, "index.html"))
+})
+
+io.on("connection", (socket) => {
+    console.log("Someone entered.")
 })
 
 server.listen(3000, () => {
